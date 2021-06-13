622b700e3e62440d5ec5364ba7c7011b9af56fa3
Dan Stoian <danmihaistoian@gmail.com>
2021-06-13 17:49:08 +0300

    firefox catched errors fixed

--- src/parser.js
+++ src/parser.js
@@ -12,24 +12,25 @@ const COLON = ':';
 
 class ReferenceParser {
 
-    prefixes = ['1','2','3'];
-
-
-    bibleBooks = [ // All 66 Protestant Bible Books
-        // Old Testmanet
-        "genesis", "exodus", "leviticus", "numbers", "deuteronomy", "joshua",
-        "judges", "ruth", "1 samuel", "2 samuel", "1 kings", "2 kings", "1 chronicles", "2 chronicles",
-        "ezra", "nehemiah", "esther", "job", "psalms", "proverbs", "ecclesiastes", "song of Solomon",
-        "isaiah", "jeremiah", "lamentations", "ezekiel", "daniel", "hosea", "joel", "amos", "obadiah",
-        "jonah", "micah", "nahum", "habakkuk", "zephaniah", "haggai", "zechariah", "malachi",
-        //New Testament
-        "matthew", "mark", "luke", "john", "acts", "romans", "1 corinthians", "2 corinthians", "galatians", "ephesians",
-        "philippians", "colossians", "1 thessalonians", "2 thessalonians", "1 timothy", "2 timothy", "titus",
-        "philemon", "hebrews", "james", "1 peter", "2 peter", "1 john", "2 john", "3 john", "jude", "revelation"];
-
+  
 
     constructor() {
         this.history = [];
+        this.numberPrefixes = ['1','2','3'];
+
+        this.wordsPrefixes = { 'first': '1', 'second': '2', 'third': '3' };
+        this.bibleBooks = [ // All 66 Protestant Bible Books
+            // Old Testmanet
+            "genesis", "exodus", "leviticus", "numbers", "deuteronomy", "joshua",
+            "judges", "ruth", "1 samuel", "2 samuel", "1 kings", "2 kings", "1 chronicles", "2 chronicles",
+            "ezra", "nehemiah", "esther", "job", "psalms", "proverbs", "ecclesiastes", "song of Solomon",
+            "isaiah", "jeremiah", "lamentations", "ezekiel", "daniel", "hosea", "joel", "amos", "obadiah",
+            "jonah", "micah", "nahum", "habakkuk", "zephaniah", "haggai", "zechariah", "malachi",
+            //New Testament
+            "matthew", "mark", "luke", "john", "acts", "romans", "1 corinthians", "2 corinthians", "galatians", "ephesians",
+            "philippians", "colossians", "1 thessalonians", "2 thessalonians", "1 timothy", "2 timothy", "titus",
+            "philemon", "hebrews", "james", "1 peter", "2 peter", "1 john", "2 john", "3 john", "jude", "revelation"];
+    
     }
 
     parseReference(bibleReference) {
@@ -46,7 +47,7 @@ class ReferenceParser {
             throw new LocalError(NotABibleReferenceError, 'Selection ${bibleReference} is not a valid Bible reference');
         }
         // if only book, add automatically chapter 1 as beginning 
-        const isBookWithNoChapter = words.length === 1 || (words.length == 2 && this.prefixes.includes(words[0]));
+        const isBookWithNoChapter = words.length === 1 || (words.length == 2 && this.numberPrefixes.includes(words[0]));
         if(isBookWithNoChapter) {
             words.push('1');
         }
@@ -62,7 +63,7 @@ class ReferenceParser {
         let potentialBibleBook;
 
         // concat prefix to bible book
-        if(this.prefixes.includes(words[0])) {
+        if(this.numberPrefixes.includes(words[0])) {
             potentialBibleBook = words.slice(0, 2).join(' ');
         } else {
             potentialBibleBook = words[0    ]
@@ -78,9 +79,9 @@ class ReferenceParser {
     }
 
     correctBookPrefix(words) {
-        const bookPrefixes = { 'first': '1', 'second': '2', 'third': '3' };
-        if (words[0] in bookPrefixes) {
-            words[0] = bookPrefixes[words[0]];
+        const 
+        if (words[0] in wordsPrefixes) {
+            words[0] = wordsPrefixes[words[0]];
         } 
     }
 }
