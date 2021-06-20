138ff48ae1ae32945a9b23bbeba8d27e9e773c56
jbus97 <hugofleitasbus@gmail.com>
2021-06-19 20:22:02 -0400

    Adds input field

--- src/action/chooseBible.js
+++ src/action/chooseBible.js
@@ -1,16 +1,29 @@
-
 const GET_VERISON = 'getVersion';
 
 const SET_VERSION = 'setVersion';
 
 const selector = document.querySelector('#bibleVersion');
 
+
 //get last selected version
-browser.runtime.sendMessage({message: GET_VERISON}).then((version) => {
-    selector.value = version;
+browser.runtime.sendsMessage({message: GET_VERISON}).then((version) => {
+    selector.value = version;   
 });
+
+document.body.style.backgroundColor = "red";
 
 //get select element
 selector.addEventListener('change', async event => {
     await browser.runtime.sendMessage({message: SET_VERSION, bible: event.target.value});
 });
+
+
+async function setTranslation(){;
+    
+    const value = document.getElementById('#bibleVersionInputField').value
+    document.getElementById('#bibleVersionInputField').value = "xd";
+    if(value in bibleVersions){
+        await browser.runtime.sendMessage({message: SET_VERSION, bible: value})
+    }    
+}
+
