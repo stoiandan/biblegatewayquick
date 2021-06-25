9e266b6c91c5e6078d3fbffa1ef4f6bae27469e7
Dan Stoian <danmihaistoian@gmail.com>
2021-06-25 20:23:44 +0300

    fetch verse

--- src/action/news_feed/xmlParser.js
+++ src/action/news_feed/xmlParser.js
@@ -8,7 +8,8 @@ const client = new XMLHttpRequest();
 (function getXMLNewsFeeD() {
     client.addEventListener('load',  resp => {
         const verseOfTheDay = client.responseXML.querySelector('content').innerHTML;
-        renderVerseOfTheDay(verseOfTheDay.slice(9,verseOfTheDay.length - 3));
+        const reference = client.responseXML.querySelector('entry > title').innerHTML;
+        renderVerseOfTheDay(verseOfTheDay.slice(9,verseOfTheDay.length - 3) + ' ' + reference);
     });
 
     client.open('GET', NEWS_FEED_URL);
